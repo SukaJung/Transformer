e1ce55be014080c587a3466519f778d5a3da4452
sukajung <tjrgk2140@naver.com>
2018-11-09 02:27:49 +0900

    server

--- server/run_model.py
+++ server/run_model.py
@@ -7,39 +7,49 @@ import os
 import numpy as np
 import io
 
+def divide_img(img):
+	imgs = []
+	for i in range(2):
+		for j in range(2):
+			imgs.append(img[256*i:256*(i+1),256*j:256*(j+1),:])
+	return np.array(imgs)
+
+def sum_img(img):
+	result_imgs = np.zeros((512,512,3),dtype=np.float32)
+	indexing = 0 
+	for i in range(2):
+		for j in range(2):
+			result_imgs[256*i:256*(i+1),256*j:256*(j+1),:] = img[indexing]
+			indexing +=1 
+	return result_imgs
+
+def run_model(sess, imgs):
+	outputs = []
+	for i in range(4):	
+		input_img = imgs[i].reshape(1,256,256,3)
+		output = sess.run("Model/g_A/t1:0", feed_dict={"input_A:0": input_img})
+		output = np.array(output)
+		outputs.append(output)
+	return np.array(outputs)
+
+def save_image(imgs):
+	result = imgs * 255
+	result = result.astype(np.uint8)
+	result = Image.fromarray(result)
+	result.save("uploads/result.jpg")
+	return;
 
 app = Flask(__name__)
 
 
-# loader = tf.train.import_meta_graph('/home/suka/resnet50/model.ckpt.meta')
-# loader.restore(sess, tf.train.latest_checkpoint('/home/suka/resnet50'))
-
-first_image = Image.open("./uploads/1.jpg")
-second_image = Image.open("./uploads/2.jpg")
-
-first_image = np.array(Image.open("./uploads/1.jpg"))
-second_image = np.array(Image.open("./uploads/2.jpg"))
-
 gpu_options = tf.GPUOptions(allow_growth=True)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
-detection_graph = tf.Graph(config=tf.ConfigProto(gpu_options=gpu_options))
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile('/home/suka/resnet50/frozen_inference_graph.pb', 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
-    
-
-run_inference_for_single_image(first_image,detection_graph)
-# MODEL = DeepLabModel("./deeplabv3_mnv2_pascal_train_aug.tar.gz")
+loader = tf.train.import_meta_graph('/home/suka/git/Transformer/server/model/cyclegan-67.meta')
+loader.restore(sess, tf.train.latest_checkpoint('/home/suka/git/Transformer/server/model'))
 
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-@app.route("/")
-def eval():
-	return "A"
 
    
 @app.route('/upload', methods=['GET', 'POST'])
@@ -47,23 +57,14 @@ def upload():
     file = request.files['file']
     img = Image.open(file)
     arr = file.stream.read()
-    img = img.resize((64,64))
+    img = img.resize((512,512))
     img.save("uploads/before.jpg")
-    img = np.asarray(img)
-    
-    img = img.reshape(1,64,64,3)
-    print(img.shape)
 
-    output = sess.run("Image-Output:0", feed_dict={"Image-Input0:0": img})
-    output = np.array(output)
-    print(output.shape)
-    output = output[0].reshape(64, 64, 3)
-    print(output.shape)
-    output = output * 255
-    output = output.astype(np.uint8)
-    result = Image.fromarray(output)
-    result.save("uploads/result.jpg")
-#     content = open("uploads/result.jpg").read()
+    img = divide_img(np.array(img))
+    outputs = run_model(sess,img)
+    outputs = outputs.reshape(4,256,256,3)
+    result = sum_img(outputs)
+    save_image(result)
     with open("uploads/result.jpg", 'rb') as bites:
         return send_file(
                      io.BytesIO(bites.read()),
